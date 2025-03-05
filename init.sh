054c160ca32554c44b8381ab6b1bb9085d0f1def
Yang Sheng Han <progamesigner@gmail.com>
2025-03-05 02:11:37 +0000

    Add init script

---
+++ init.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+set -u
+
+secrets=(
+components/tunnels.map
+components/cloudflare.env
+components/cloudflared/cloudflared.env
+components/coder/coder.env
+components/dify/dify.env
+components/rsshub/rsshub.env
+components/tailscale/protonvpn.env
+components/tailscale/tailscale.env
+)
+
+for secret in $secrets; do
+    touch $secret
+    curl -sSL $SECRET_MANAGER_API/$secret > $secret
+done
